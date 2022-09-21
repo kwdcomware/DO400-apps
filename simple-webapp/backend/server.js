94d05b3d549bc11f1f0485b3a1d2410ca46a919a
Student User <student@workstation.lab.example.com>
2022-09-21 05:39:42 -0400

    Refactor

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -1,9 +1,9 @@
 const http = require("http");
 const url = require("url");
-const greeting = greet(name);
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
+    const greeting = greet(name);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
