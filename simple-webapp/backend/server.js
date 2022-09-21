1d65345fa8cc8c71b42a0f621352c8a99e76dbd0
Student User <student@workstation.lab.example.com>
2022-09-21 06:01:12 -0400

    Refactor-2

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const url = require("url");
+const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
